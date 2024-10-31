4c8d27bee0dab43a2da89465207df64d548c6c43
PeuAlmeida <pedroalmeida.programador@gmail.com>
2024-10-30 22:15:34 -0300

    Adicionado o MirageJS e o Axios

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,5 +1,5 @@
 import { Summary } from "../Summary";
-import TransactionTable from "../TransactionTable";
+import {TransactionTable} from "../TransactionTable";
 import { Container } from "./styles";
 
 export function Dashboard() {
